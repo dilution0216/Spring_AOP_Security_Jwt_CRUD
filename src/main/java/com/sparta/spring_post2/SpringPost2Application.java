e3309f5aa460a418d00e2339b1a2a74909e5bfbb
dilution0216 <hglad0216@naver.com>
2023-07-11 01:31:58 +0900

    git init 인텔리제이 설정

---
+++ src/main/java/com/sparta/spring_post2/SpringPost2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_post2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPost2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPost2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spring_post2/SpringPost2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_post2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPost2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
